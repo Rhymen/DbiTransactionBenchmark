c28943ad208a2750b410ab4e367861088ee627a2
Marcel <marcel.kocks@hotmail.de>
2018-01-11 13:36:42 +0100

    fDeposit optimization

--- db/functions/fDeposit.sql
+++ db/functions/fDeposit.sql
@@ -3,19 +3,19 @@ CREATE OR REPLACE FUNCTION fDeposit(pAccId INTEGER, pTellerId INTEGER, pBranchId
 DECLARE
   newBalance INTEGER;
 BEGIN
-  UPDATE branches
-  SET balance = balance + pDelta
-  WHERE branchid = pBranchId;
-
-  UPDATE tellers
-  SET balance = balance + pDelta
-  WHERE tellerid = pTellerId;
-
   UPDATE accounts
   SET balance = balance + pDelta
   WHERE accid = pAccId
   RETURNING balance
     INTO newBalance;
+
+  UPDATE tellers
+  SET balance = balance + pDelta
+  WHERE tellerid = pTellerId;
+
+  UPDATE branches
+  SET balance = balance + pDelta
+  WHERE branchid = pBranchId;
 
   INSERT INTO history (accid, tellerid, delta, branchid, accbalance, cmmnt)
   VALUES (pAccId, pTellerId, pDelta, pBranchId, newBalance, '');
